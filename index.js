f8dfc42b575ab55aef9a835d7ade010303878e73
Symion Edwards <symion.edwards@gmail.com>
2020-04-28 09:27:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,8 +21,15 @@ function total() {
   // write your code here
 }
 
-function removeFromCart(item) {
-  // write your code here
+function removeFromCart(itemName) {
+  var newcart = cart;
+  for (let i = 0; i < cart.length; i++) {
+    if (cart[i].hasOwnProperty(itemName)) {
+      return newcart.splice(i, 1);
+    }
+  }
+
+  return console.log("That item is not in your cart.");
 }
 
 function placeOrder(cardNumber) {
