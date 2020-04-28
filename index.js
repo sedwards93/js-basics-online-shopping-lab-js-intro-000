72c2271316bc41c1d9f504714a41fb1d49e0992b
Symion Edwards <symion.edwards@gmail.com>
2020-04-28 09:51:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ function total() {
 
 function removeFromCart(item) {
   for (let i = 0; i < cart.length; i++) {
-    if (cart[i].hasOwnProperty(item)) {
+    if (cart[i].item) {
       return cart.splice(i, 1);
     }
   }
